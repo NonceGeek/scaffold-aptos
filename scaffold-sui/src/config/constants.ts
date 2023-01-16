90cc131f50780da6a06773f8656f896e993591b2
qiwihui <qwh005007@gmail.com>
2023-01-16 10:57:30 +0800

    fix: rename to NEXT_PUBLIC_SUI_NETWORK

--- scaffold-sui/src/config/constants.ts
+++ scaffold-sui/src/config/constants.ts
@@ -6,6 +6,6 @@ export const DAPP_NAME = process.env.NEXT_PUBLIC_DAPP_NAME!; // changed here.
 export const DAPP_ADDRESS = process.env.NEXT_PUBLIC_DAPP_ADDRESS!; // changed here.
 export const MARKET_COINT_TYPE = process.env.NEXT_PUBLIC_MARKET_COIN_TYPE!;
 
-export const NETWORK=process.env.NEXT_PUBLIC_APTOS_NETWORK!;
+export const NETWORK=process.env.NEXT_PUBLIC_SUI_NETWORK!;
 
 export const MODULE_URL="https://explorer.aptoslabs.com/account/" + DAPP_ADDRESS + "/modules?network=" + NETWORK
