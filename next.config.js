55e60c64b19d3e58b39f4cd052912054ec9c20dd
何伟 <cnhwvista@gmail.com>
2023-04-21 23:52:54 +0800

    ignore eslint error during build

--- next.config.js
+++ next.config.js
@@ -24,6 +24,11 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, path: false };
     return config;
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
